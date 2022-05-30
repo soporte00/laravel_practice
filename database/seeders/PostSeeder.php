dc8359c5987ff9ba1be629c5e0c54a78329a6435
David <david_ortiz87@live.com.ar>
2022-05-30 10:48:27 -0300

    created post model factory and seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,20 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \App\Models\Post;
 
-class DatabaseSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
-        $this->call([
-            PostSeeder::class,
-        ]);
-        
+        Post::factory(20)->create();
     }
 }
